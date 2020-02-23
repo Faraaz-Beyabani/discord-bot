12f94860c06a76bf818bc08bf4a2a26560846d7d
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-02-22 23:15:40 -0600

    1

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import os
+import random
 
+from bs4 import BeautifulSoup
+import requests
 import discord
 from discord.ext import commands 
 
@@ -8,9 +11,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = commands.Bot(command_prefix = 'p')
-
+client = commands.Bot(command_prefix = 'owo')
 token = os.environ['BOT_TOKEN']
+yt_list = requests.get('https://www.youtube.com/playlist?list=PLQrjHunXuYt7i9uaxZIdxinu71JjXY7LG')
+soup = [a['href'] for a in BeautifulSoup(yt_list).body.find_all('a', class_='yt-simple-endpoint', href=True)]
 
 @client.event
 async def on_ready():
@@ -23,8 +27,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if not message.author.bot:
-        channel = message.channel
-        await channel.send(message.content)
+        if message.content.startswith('owoplay'):
+            await channel.send('youtube.com/'+random.choice(soup))
+
 
     await client.process_commands(message)
 
