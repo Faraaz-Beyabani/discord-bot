b83618c1e80a700b31aa098c76bc7308255d09dc
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-02-22 23:32:32 -0600

    playlist randomizer

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ load_dotenv()
 client = commands.Bot(command_prefix = 'owo')
 token = os.environ['BOT_TOKEN']
 yt_list = requests.get('https://www.youtube.com/playlist?list=PLQrjHunXuYt7i9uaxZIdxinu71JjXY7LG')
-soup = [a['href'] for a in BeautifulSoup(yt_list.text, 'html.parser').body.find_all('a', class_='yt-simple-endpoint', href=True)]
+soup = [a['href'] for a in BeautifulSoup(yt_list.text, 'html.parser').body.find_all('a',{'class':'pl-video-title-link'})]
 
 @client.event
 async def on_ready():
@@ -27,8 +27,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if not message.author.bot:
+        channel = message.channel
         if message.content.startswith('owoplay'):
-            await channel.send('youtube.com/'+random.choice(soup))
+            await channel.send('https://www.youtube.com'+random.choice(soup))
 
 
     await client.process_commands(message)
