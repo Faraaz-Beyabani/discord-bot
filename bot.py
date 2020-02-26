459c36a77e2beec395653e6a78a99f305d507bf7
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-02-26 15:49:59 -0600

    natural language processing

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import re
 import random
 
 from bs4 import BeautifulSoup
@@ -40,32 +41,44 @@ async def on_ready():
 	except Exception as e:
 		print(e)
 
-@client.command(pass_context=True)
-async def play(ctx):
-    yt_list = requests.get('https://www.youtube.com/playlist?list=PLQrjHunXuYt7i9uaxZIdxinu71JjXY7LG')
-    soup = [a['href'] for a in BeautifulSoup(yt_list.text, 'html.parser').body.find_all('a',{'class':'pl-video-title-link'})]
-    url = ('https://www.youtube.com'+random.choice(soup)).split('&list')[0]
-    vc = None
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('bot'):
+        if re.search('am i', message.content.lower()):
+            await message.channel.send('yep, you are')
+        elif re.search(r'do[n\'t]*? u', message.content.lower().replace('you', 'u')):
+            await message.channel.send('yep, i do')
+        elif match := re.search(r'is [a-zA-z]*?', message.content.lower()):
+            await message.channel.send(f'yep, {message.content.split()[2]} is {" ".join(message.content.split()[3:])}')
 
-    voice_channel =  ctx.message.author.voice
-    if not voice_channel or not voice_channel.channel:
-        await ctx.send("You are not connected to a voice channel, idiot.\nhttps://tenor.com/r4Hd.gif")
-        return
-    elif client.voice_clients:
-        vc = await voice_channel.move_to()
+    await client.process_commands(message)
 
-    loop = asyncio.get_event_loop()
-    data = await loop.run_in_executor(None, lambda: ydl.extract_info(url, download=False))
-    filename = data['url']
+# @client.command(pass_context=True)
+# async def play(ctx):
+#     yt_list = requests.get('https://www.youtube.com/playlist?list=PLQrjHunXuYt7i9uaxZIdxinu71JjXY7LG')
+#     soup = [a['href'] for a in BeautifulSoup(yt_list.text, 'html.parser').body.find_all('a',{'class':'pl-video-title-link'})]
+#     url = ('https://www.youtube.com'+random.choice(soup)).split('&list')[0]
+#     vc = None
 
-    vc = await voice_channel.channel.connect()
-    vc.play(discord.FFmpegPCMAudio(filename, executable='./ffmpeg/bin/ffmpeg.exe', **{'options': '-vn'}))
-    
-@client.command(pass_context = True)
-async def stop(ctx):
-    for x in client.voice_clients:
-        return await x.disconnect()
+#     voice_channel =  ctx.message.author.voice
+#     if not voice_channel or not voice_channel.channel:
+#         await ctx.send("You are not connected to a voice channel, idiot.\nhttps://tenor.com/r4Hd.gif")
+#         return
+#     elif client.voice_clients:
+#         vc = await voice_channel.move_to()
 
-    return await ctx.send("You or I are not connected to any voice channel on this server... worgen.\nhttps://tenor.com/r4Hd.gif")
+#     loop = asyncio.get_event_loop()
+#     data = await loop.run_in_executor(None, lambda: ydl.extract_info(url, download=False))
+#     filename = data['url']
+
+#     vc = await voice_channel.channel.connect()
+#     vc.play(discord.FFmpegPCMAudio(filename, executable='./ffmpeg/bin/ffmpeg.exe', **{'options': '-vn'}))
+
+# @client.command(pass_context = True)
+# async def stop(ctx):
+#     for x in client.voice_clients:
+#         return await x.disconnect()
+
+#     return await ctx.send("You or I are not connected to any voice channel on this server... worgen.\nhttps://tenor.com/r4Hd.gif")
 
 client.run(token)
