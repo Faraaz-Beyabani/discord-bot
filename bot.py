4dfdc6eea01e45de37009317b9c9b7bc76eebce6
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-02-26 18:16:15 -0600

    does

--- bot.py
+++ bot.py
@@ -50,40 +50,17 @@ async def on_message(message):
             await message.channel.send(f'yes i am {" ".join(message.content.split()[3:])}')
         elif re.search(r'do[n\'t]*? u', message.content.lower().replace('you', 'u')):
             await message.channel.send('yep, i do')
-        elif match := re.search(r'is [a-zA-z]*?', message.content.lower()):
+        elif re.search(r'does[n\'t]*? [a-zA-Z]', message.content.lower()):
             if message.content.split()[2] in ['your', 'the']:
-                await message.channel.send(f'yep, {" ".join(message.content.split()[2:4]).replace("yours", "mine").replace("your", "my").replace("you", "i")} is {" ".join(message.content.split()[4:])}')
+                await message.channel.send(f'yep, {" ".join(message.content.split()[2:4]).replace("yours", "mine").replace("your", "my").replace("you", "i")} does {" ".join(message.content.split()[4:]).replace("yours", "mine").replace("your", "my").replace("you", "me")}')
+            else:
+                await message.channel.send(f'yep, {message.content.split()[2].replace("yours", "mine").replace("your", "my").replace("you", "i")} does {" ".join(message.content.split()[3:]).replace("yours", "mine").replace("your", "my").replace("you", "i")}')
+        elif match := re.search(r'is [a-zA-Z]*?', message.content.lower()):
+            if message.content.split()[2] in ['your', 'the']:
+                await message.channel.send(f'yep, {" ".join(message.content.split()[2:4]).replace("yours", "mine").replace("your", "my").replace("you", "i")} is {" ".join(message.content.split()[4:]).replace("yours", "mine").replace("your", "my").replace("you", "me")}')
             else:
                 await message.channel.send(f'yep, {message.content.split()[2].replace("yours", "mine").replace("your", "my").replace("you", "i")} is {" ".join(message.content.split()[3:]).replace("yours", "mine").replace("your", "my").replace("you", "i")}')
 
     await client.process_commands(message)
-
-# @client.command(pass_context=True)
-# async def play(ctx):
-#     yt_list = requests.get('https://www.youtube.com/playlist?list=PLQrjHunXuYt7i9uaxZIdxinu71JjXY7LG')
-#     soup = [a['href'] for a in BeautifulSoup(yt_list.text, 'html.parser').body.find_all('a',{'class':'pl-video-title-link'})]
-#     url = ('https://www.youtube.com'+random.choice(soup)).split('&list')[0]
-#     vc = None
-
-#     voice_channel =  ctx.message.author.voice
-#     if not voice_channel or not voice_channel.channel:
-#         await ctx.send("You are not connected to a voice channel, idiot.\nhttps://tenor.com/r4Hd.gif")
-#         return
-#     elif client.voice_clients:
-#         vc = await voice_channel.move_to()
-
-#     loop = asyncio.get_event_loop()
-#     data = await loop.run_in_executor(None, lambda: ydl.extract_info(url, download=False))
-#     filename = data['url']
-
-#     vc = await voice_channel.channel.connect()
-#     vc.play(discord.FFmpegPCMAudio(filename, executable='./ffmpeg/bin/ffmpeg.exe', **{'options': '-vn'}))
-
-# @client.command(pass_context = True)
-# async def stop(ctx):
-#     for x in client.voice_clients:
-#         return await x.disconnect()
-
-#     return await ctx.send("You or I are not connected to any voice channel on this server... worgen.\nhttps://tenor.com/r4Hd.gif")
 
 client.run(token)
