6864c6bc50e9b6dffab84c023fdb8a77f35b2fba
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-03-21 20:19:01 -0500

    Finished basic name generator

--- bot.py
+++ bot.py
@@ -79,4 +79,91 @@ async def roll(ctx):
 async def flip(ctx):
     await ctx.send(random.choice(['heads', 'tails']))
 
+@client.command(pass_context=True)
+async def npc(ctx):
+    races = {
+        'elf': ['ara', 'be', 'tha', 'dan', 'gali', 'var', 'lego', 'las', 'vol', 'lynn', 'eth', 'riel', 'kiir', 'enna', 'and', 'mor', 'vor'],
+        'human': ['del', 'vik', 'thaz', 'mith', 'gan', 'frey', 'mal', 'ath', 'ina', 'arra', 'et', 'ram', 'kina', 'ang', 'der'],
+        'dragonborn':  ['ash', 'kry', 'iv', 'gar', 'cleth', 'ava', 'har', 'kor', 'inn', 'verth', 'yarj', 'fek', 'jho', 'mah', 'un', 'ghe', 'ina'],
+        'gnome': ['ook', 'nock', 'nam', 'oodle', 'vyn', 'kin', 'fun', 'berg', 'bimp', 'sche', 'kle', 'loop', 'aard', 'bree', 'illa', 'froo'],
+        'orc': ['kre', 'nur', 'nor', 'ogg', 'gogg', 'ukk', 'thar', 'lug', 'mur', 'uda', 'sha', 'mog', 'gûl', 'ok', 'grum', 'mhu', 'rex'],
+        'dwarf': ['rich', 'din', 'rin', 'albe', 'arge', 'wynn', 'hild', 'gar', 'bru', 'nor', 'thor', 'jörn', 'rik', 'rim'],
+        'halfling': ['rin', 'mer', 'ret', 'dri', 'tia', 'os', 'born', 'by', 'mia', 'sera', 'ela', 'jil', 'ver'],
+        'tiefling': ['bara', 'kas', 'kis', 'nos', 'am', 'mal', 'isto', 'seis', 'kal', 'ista', 'mon', 'ana', 'rai', 'kos', 'mak'],
+    }
+    message = ctx.message.content
+    name = ''
+
+    if 'half-elf' in message:
+        choices = races['elf'] + races['human']
+        num = random.randint(2, 4)
+        for i in range(num):
+            part = random.choice(choices)
+            while part in name:
+                part = random.choice(choices)
+            name += part
+    elif 'elf' in message:
+        num = random.randint(2, 4)
+        for i in range(num):
+            part = random.choice(races['elf'])
+            while part in name:
+                part = random.choice(races['elf'])
+            name += part
+    elif 'human' in message:
+        num = random.randint(2, 3)
+        for i in range(num):
+            part = random.choice(races['human'])
+            while part in name:
+                part = random.choice(races['human'])
+            name += part
+    elif 'dragon' in message:
+        num = random.randint(2, 3)
+        for i in range(num):
+            part = random.choice(races['dragonborn'])
+            while part in name:
+                part = random.choice(races['dragonborn'])
+            name += part
+    elif 'gnome' in message:
+        num = random.randint(2, 4)
+        for i in range(num):
+            part = random.choice(races['gnome'])
+            while part in name:
+                part = random.choice(races['gnome'])
+            name += part
+    elif 'orc' in message:
+        num = random.randint(1, 2)
+        for i in range(num):
+            part = random.choice(races['orc'])
+            while part in name:
+                part = random.choice(races['orc'])
+            name += part
+    elif 'dwarf' in message:
+        num = random.randint(2, 3)
+        for i in range(num):
+            part = random.choice(races['dwarf'])
+            while part in name:
+                part = random.choice(races['dwarf'])
+            name += part
+    elif 'half' in message:
+        num = 2
+        for i in range(num):
+            part = random.choice(races['halfling'])
+            while part in name:
+                part = random.choice(races['halfling'])
+            name += part
+    elif 'tief' in message:
+        num = 2
+        for i in range(num):
+            part = random.choice(races['tiefling'])
+            while part in name:
+                part = random.choice(races['tiefling'])
+            name += part
+
+    match = re.search(r'(a|e|i|o|u)\1+', name)
+    if match:
+        name = re.sub(match.group(), match.group()[0]*2, name)
+
+    await ctx.send(name.capitalize())
+
+
 client.run(token)
