69995e842fe1f0383777950fc279af3cc6c5c515
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-03-21 21:27:17 -0500

    Add converse interface

---
+++ message.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+channelID = "691100088004771910"
+botToken = os.environ['BOT_TOKEN']
+
+baseURL = "https://discordapp.com/api/channels/{}/messages".format(channelID)
+headers = { "Authorization":"Bot {}".format(botToken),
+            "User-Agent":"myBotThing (http://some.url, v0.1)",
+            "Content-Type":"application/json", }
+
+while True:
+    message = input()
+    POSTedJSON =  json.dumps ( {"content":message} )
+    r = requests.post(baseURL, headers = headers, data = POSTedJSON)
